8e549b1723626226418f9a7a3d90fae4a2222b2e
jriverosesma <jriverosema@gmail.com>
2023-12-08 01:18:22 +0100

    test: update test_unet

--- test/test_unet.py
+++ test/test_unet.py
@@ -3,23 +3,23 @@ import torch
 from torch import nn
 
 # Tumor SemSeg
-from tumor_semseg.architecture.unet import UNet
+from tumor_semseg.architecture.unet import UNet, UNetConfig
 
 
 @pytest.mark.parametrize(
-    "input_shape, n_classes, activation",
+    "input_shape, config",
     [
-        ((5, 3, 256, 256), 10, nn.ReLU(inplace=True)),
-        ((1, 3, 256, 256), 5, nn.ReLU(inplace=True)),
-        ((10, 1, 256, 256), 1, nn.Sigmoid()),
-        ((10, 1, 64, 64), 1, nn.Sigmoid()),
-        ((10, 1, 256, 128), 3, nn.Sigmoid()),
+        ((5, 3, 256, 256), UNetConfig(10, 3, nn.ReLU(inplace=True))),
+        ((1, 3, 256, 256), UNetConfig(5, 3, nn.ReLU(inplace=True))),
+        ((10, 1, 256, 256), UNetConfig(7, 1, nn.Sigmoid())),
+        ((10, 1, 64, 64), UNetConfig(4, 1, nn.Sigmoid())),
+        ((10, 1, 256, 128), UNetConfig(1, 1, nn.Sigmoid())),
     ],
 )
-def test_unet(input_shape, n_classes, activation):
-    net = UNet(n_classes, input_shape[1], activation)
+def test_unet(input_shape, config):
+    net = UNet(config)
     output = net(torch.rand(input_shape))
 
-    expected_output_shape = (input_shape[0], n_classes, input_shape[2], input_shape[3])
+    expected_output_shape = (input_shape[0], config.n_classes, input_shape[2], input_shape[3])
 
     assert output.shape == expected_output_shape
