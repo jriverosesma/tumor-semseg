6fd177096bd1d98fea3ecec9a0054ef19612ae27
jriverosesma <jriverosema@gmail.com>
2023-12-10 14:58:30 +0100

    feat: add augmentations

--- tumor_semseg/dataset/brain_mri.py
+++ tumor_semseg/dataset/brain_mri.py
@@ -15,26 +15,37 @@ from PIL import Image
 from PIL.Image import Resampling
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader, Dataset
-from torchvision import transforms
+from torchvision.transforms import transforms
+
+# Tumor SemSeg
+from tumor_semseg.dataset.augmentations import get_training_augmentations
 
 
 @dataclass
 class BrainMRIDataModuleConfig:
-    dataset_dir: Path
+    dataset_dirpath: Path
     seed: Optional[int] = None
     test_size: float = 0.2
-    image_size: list[int] = field(default_factory=lambda: [256, 256])  # HxW
+    image_size: tuple[int, int] = field(default_factory=lambda: (256, 256))  # HxW
     batch_size: int = 8
     num_workers: int = 2
+    augment: bool = True
     drop_last: bool = False
 
 
 class BrainMRIDataset(Dataset):
-    def __init__(self, images: list[str], masks: list[str], image_size=tuple[256, 256]):
+    def __init__(
+        self, images: list[str], masks: list[str], augment: bool = True, image_size: tuple[int, int] = (256, 256)
+    ):
         self.images = images
         self.masks = masks
         self.image_size = image_size
-        self.transform = transforms.Compose([transforms.ToTensor()])
+        self.augment = augment
+        self.augmentation_pipeline = get_training_augmentations(image_size)
+        self.image_transform = transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]
+        )
+        self.mask_transform = transforms.ToTensor()
 
     def __len__(self):
         return len(self.images)
@@ -42,10 +53,14 @@ class BrainMRIDataset(Dataset):
     def __getitem__(self, idx):
         image = Image.open(self.images[idx]).convert("RGB").resize(self.image_size, resample=Resampling.BILINEAR)
         mask = Image.open(self.masks[idx]).resize(self.image_size, resample=Resampling.NEAREST)
-        image = np.array(image).astype(np.float32)
-        mask = np.array(mask).astype(np.float32) / 255
+        image = np.array(image)
+        mask = np.array(mask).astype(np.float32) / 255.0
 
-        return self.transform(image), self.transform(mask)
+        if self.augment:
+            augmented = self.augmentation_pipeline(image=image, mask=mask)
+            image, mask = augmented["image"], augmented["mask"]
+
+        return self.image_transform(image), self.mask_transform(mask)
 
 
 class BrainMRIDataModule(L.LightningDataModule):
@@ -58,8 +73,10 @@ class BrainMRIDataModule(L.LightningDataModule):
             self.train = BrainMRIDataset(self.x_train, self.y_train, self.config.image_size)
             self.val = BrainMRIDataset(self.x_val, self.y_val, self.config.image_size)
 
-    def prepare_data(self) -> None:
-        mask_filepaths = glob.glob(str(self.config.dataset_dir) + "/**/*_mask.tif", recursive=True)
+    def prepare_data(self):
+        assert (self.config.dataset_dirpath / "kaggle_3m").exists()
+
+        mask_filepaths = glob.glob(str(self.config.dataset_dirpath) + "/kaggle_3m/**/*_mask.tif", recursive=True)
         image_filepaths = [path.replace("_mask", "") for path in mask_filepaths]
 
         df = pd.DataFrame(data={"image_filepaths": image_filepaths, "mask_filepaths": mask_filepaths})
