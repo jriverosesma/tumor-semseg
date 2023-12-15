9fdfecfb25088b9cf65ab7f35056041a1c317703
jriverosesma <juan.rivero-sesma@airbus.com>
2023-12-15 18:19:24 +0000

    update

--- tumor_semseg/optimizer/base_optimizer.py
+++ tumor_semseg/optimizer/base_optimizer.py
@@ -10,17 +10,19 @@ import torch
 
 @dataclass
 class BaseOptimizer:
-    optimizer_class: Type[torch.optim.Optimizer]
-    optimizer_params: dict[str, Any] = field(default_factory=lambda: {})
+    optimizer_class: Type[torch.optim.Optimizer] = torch.optim.Adamax
+    params: dict[str, Any] = field(default_factory=lambda: {"lr": 1e-3})
 
     def get_optimizer(self, model: torch.nn.Module):
-        return self.optimizer_class(model.parameters(), **self.optimizer_params)
+        return self.optimizer_class(model.parameters(), **self.params)
 
 
 @dataclass
 class BaseScheduler:
-    scheduler_class: Type[torch.optim.lr_scheduler.LRScheduler]
-    scheduler_params: dict[str, Any] = field(default_factory=lambda: {})
+    scheduler_class: Type[torch.optim.lr_scheduler.LRScheduler] = torch.optim.lr_scheduler.LambdaLR
+    params: dict[str, Any] = field(
+        default_factory=lambda: {"lr_lambda": [lambda epoch: epoch // 30, lambda epoch: 0.95**epoch]}
+    )
 
-    def get_scheduler(self, optimizer):
-        return self.scheduler_class(optimizer, **self.scheduler_params)
+    def get_scheduler(self, optimizer: torch.optim.Optimizer):
+        return self.scheduler_class(optimizer, **self.params)
