b2490447bfacfcacfdb90cbc1c091474a3cf46b1
jriverosesma <juan.rivero-sesma@airbus.com>
2023-12-16 01:54:30 +0000

    update

--- test/test_deeplab.py
+++ test/test_deeplab.py
@@ -11,10 +11,10 @@ from tumor_semseg.architecture.deeplab import DeepLabv3, DeepLabv3Config
     [
         ((2, 3, 256, 256), DeepLabv3Config(10, 3, "mobilenet", True)),
         ((2, 3, 256, 256), DeepLabv3Config(5, 3, "mobilenet", False)),
-        ((2, 3, 256, 256), DeepLabv3Config(10, 3, "deeplabv3_resnet50", False)),
-        ((2, 3, 256, 256), DeepLabv3Config(5, 3, "deeplabv3_resnet50", True)),
-        ((2, 3, 256, 256), DeepLabv3Config(10, 3, "deeplabv3_resnet101", False)),
-        ((2, 3, 256, 256), DeepLabv3Config(5, 3, "deeplabv3_resnet101", True)),
+        ((2, 3, 256, 256), DeepLabv3Config(10, 3, "resnet50", False)),
+        ((2, 3, 256, 256), DeepLabv3Config(5, 3, "resnet50", True)),
+        ((2, 3, 256, 256), DeepLabv3Config(10, 3, "resnet101", False)),
+        ((2, 3, 256, 256), DeepLabv3Config(5, 3, "resnet101", True)),
     ],
 )
 def test_deeplab(input_shape, config):
