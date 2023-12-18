57df705c4db2aabd075be5d0e330f6524109c0fa
jriverosesma <juan.rivero-sesma@airbus.com>
2023-12-18 18:05:39 +0000

    update

--- tumor_semseg/module/callbacks.py
+++ tumor_semseg/module/callbacks.py
@@ -58,19 +58,18 @@ class PredVisualizationCallback(L.Callback):
                     aim.Image(fig),
                     name="image",
                     epoch=trainer.current_epoch,
-                    step=batch_idx,
                     context={"subset": "train"},
                 )
 
     @rank_zero_only
-    def on_val_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
+    def on_validation_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
         if batch_idx % min(self.log_every_n_batches, trainer.num_val_batches) == 0:
             for i in range(min(self.num_samples, batch[0].size(0))):
                 fig = PredVisualizationCallback.generate_pred_visualization(
                     batch[0][i], batch[1][i], outputs["pred"][i], pl_module.bin_det_threshold
                 )
                 trainer.logger.experiment.track(
-                    aim.Image(fig), name="image", epoch=trainer.current_epoch, step=batch_idx, context={"subset": "val"}
+                    aim.Image(fig), name="image", epoch=trainer.current_epoch, context={"subset": "val"}
                 )
 
 
@@ -84,7 +83,7 @@ class ComputeIoUCallback(L.Callback):
             pl_module.log(f"train_IoU_class_{class_idx}", class_iou, sync_dist=True)
         pl_module.log("train_mIoU", iou.mean(), sync_dist=True, prog_bar=True)
 
-    def on_val_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
+    def on_validation_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
         _, y = batch
         y_hat = outputs["pred"]
         iou = compute_iou(y_hat, y).mean(0)  # Average over batches
