d1383927c50d6d98c8148e2df7476d616f150a8c
jriverosesma <juan.rivero-sesma@airbus.com>
2023-12-18 16:54:10 +0000

    update

--- tumor_semseg/module/callbacks.py
+++ tumor_semseg/module/callbacks.py
@@ -1,10 +1,11 @@
 # Third-Party
 import aim
 import lightning as L
-import torch
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn.functional as F
 from lightning.pytorch.utilities import rank_zero_only
 from torch import Tensor
-from torchvision.utils import make_grid
 
 # TumorSemSeg
 from tumor_semseg.loss.utils import compute_iou
@@ -17,32 +18,48 @@ class PredVisualizationCallback(L.Callback):
         self.num_samples = num_samples
 
     @staticmethod
-    def generate_pred_grid(x: Tensor, y: Tensor, y_hat: Tensor):
-        y *= 255
-        y_hat *= 255
-        y = y.repeat_interleave(3, dim=1)
-        y_hat = y_hat.repeat_interleave(3, dim=1)
-        overlay = 0.7 * x + 0.3 * y
+    def generate_pred_visualization(x: Tensor, y: Tensor, y_hat: Tensor, bin_det_threshold: float):
+        norm_image = (x - x.min()) / (x.max() - x.min())
+        norm_image = 255 * norm_image.permute(1, 2, 0)
+        mask = 255 * y
+        pred = y_hat.float().detach().numpy().squeeze(0)
+        tpred = np.zeros(pred.shape)
+        tpred[pred > bin_det_threshold] = 1.0
 
-        grid = torch.cat((x, y, y_hat, overlay), dim=0)
+        fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(10, 10))
 
-        return make_grid(grid, nrow=4, padding=10, pad_value=255)
+        ax[0, 0].imshow(norm_image)
+        ax[0, 0].set_title("image")
+        ax[0, 1].imshow(mask)
+        ax[0, 1].set_title("mask")
+        ax[1, 0].imshow(pred)
+        ax[1, 0].set_title("prediction")
+        ax[1, 1].imshow(tpred)
+        ax[1, 1].set_title("thresholded prediction")
+
+        return fig
 
     @rank_zero_only
     def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
-        if batch_idx % min(self.log_every_n_batches, trainer.num_training_batches - 1) == 0:
-            grid = PredVisualizationCallback.generate_pred_grid(batch[0], batch[1], outputs["pred"])
-            trainer.logger.experiment.track(
-                aim.Image(grid, caption="Image Training"), name="train", context={"context_key": "train_value"}
-            )
+        if batch_idx % min(self.log_every_n_batches, trainer.num_training_batches) == 0:
+            for i in range(min(self.num_samples, batch[0].size(0))):
+                fig = PredVisualizationCallback.generate_pred_visualization(
+                    batch[0][i], batch[1][i], outputs["pred"][i], pl_module.bin_det_threshold
+                )
+                trainer.logger.experiment.track(
+                    aim.Image(fig, caption="Image Training"), name="train", context={"context_key": "train_value"}
+                )
 
     @rank_zero_only
     def on_val_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
-        if batch_idx % min(self.log_every_n_batches, trainer.num_training_batches - 1) == 0:
-            grid = PredVisualizationCallback.generate_pred_grid(batch[0], batch[1], outputs["pred"])
-            trainer.logger.experiment.track(
-                aim.Image(grid, caption="Image Validation"), name="val", context={"context_key": "val_value"}
-            )
+        if batch_idx % min(self.log_every_n_batches, trainer.num_val_batches) == 0:
+            for i in range(min(self.num_samples, batch[0].size(0))):
+                fig = PredVisualizationCallback.generate_pred_visualization(
+                    batch[0][i], batch[1][i], outputs["pred"][i], pl_module.bin_det_threshold
+                )
+                trainer.logger.experiment.track(
+                    aim.Image(fig, caption="Image Validation"), name="train", context={"context_key": "val_value"}
+                )
 
 
 class ComputeIoUCallback(L.Callback):
