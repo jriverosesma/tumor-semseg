a252e9a5bfb3bf1daf6acc935f410373bfa7a89a
jriverosesma <juan.rivero-sesma@airbus.com>
2023-12-23 10:45:36 +0000

    update

--- tumor_semseg/optimize/scheduler.py
+++ tumor_semseg/optimize/scheduler.py
@@ -27,7 +27,7 @@ class CustomScheduler:
 reduce_lr_on_plateau: CustomScheduler = partial(
     CustomScheduler,
     ReduceLROnPlateau,
-    {"mode": "max", "factor": 0.8, "patience": 2, "threshold": 5e-4},
+    {"mode": "max", "factor": 0.2, "patience": 2, "threshold": 5e-4},
     {"monitor": "val_mIoU"},
 )
 lambda_lr: CustomScheduler = partial(CustomScheduler, LambdaLR, {"lr_lambda": lambda epoch: 0.85**epoch})
